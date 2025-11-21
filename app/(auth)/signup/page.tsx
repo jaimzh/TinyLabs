8a4761083d11de98c10db23fc8337ef0c8fe5bcd
jaimzh <jameshenshaw10@gmail.com>
2025-11-21 01:59:35 +0100

    feat: signup page boilerplate

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,11 @@
-export default function SignUpPage() {
+import { SignupForm } from "@/components/signup-form"
+
+export default function Page() {
   return (
-    <div>
-      signup page
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm />
+      </div>
     </div>
-  );
+  )
 }
